7085411cd45cf5578aa4661fceba3cb8bb9d72b0
Bicanic-Alen <bicanic.alen@iisgalvanimi.edu.it>
2020-03-15 13:41:26 +0000

    2.3

--- pren/src/app/app.module.ts
+++ pren/src/app/app.module.ts
@@ -4,10 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-
-
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-
 import { BookingList } from './bookingList.model';
 import { Booking } from './booking.model';
 import { Room } from './room.model';
@@ -15,11 +12,7 @@ import { Room } from './room.model';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    Booking,
-    BookingList,
-    Room
-
+    AppComponent
   ],
   imports: [
     BrowserModule,
