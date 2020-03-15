5900ec900d687dc1ac59f36d06f50585b1c3fe3a
Bicanic-Alen <bicanic.alen@iisgalvanimi.edu.it>
2020-03-15 18:56:04 +0000

    4.4

--- pren/src/app/room.model.ts
+++ pren/src/app/room.model.ts
@@ -4,7 +4,7 @@ import { Booking } from './booking.model';
 export class Room
 {
   constructor(
-    public penotazione : Booking,
+    public booking : Booking,
     public from: Date,
     public to : Date,
     public name : String,
